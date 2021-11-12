e676e534b3216c877b7929f53c1000c32b361bce
safarpeter <safarpeter96@gmail.com>
2021-11-12 14:05:10 +0100

    scraper script updated

--- youtube.py
+++ youtube.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
@@ -16,6 +15,7 @@ browser.find_element_by_xpath('/html/body/ytd-app/ytd-consent-bump-v2-lightbox/t
 time.sleep(2)
 prev_height = browser.execute_script("return document.documentElement.scrollHeight")
 
+#scrolling to the bottom of the page
 while True:
     browser.execute_script('window.scrollTo(0, document.documentElement.scrollHeight);')
     time.sleep(2)
@@ -23,7 +23,7 @@ while True:
     new_height = browser.execute_script("return document.documentElement.scrollHeight")
 
     if new_height == prev_height:
-        print("Végiggörgettük az oldalt!")
+        print("We have reached the bottom of the page successfully!")
         break
     prev_height = new_height
 
@@ -50,33 +50,32 @@ print(type(titles_raw))
 for i in range(len(titles_raw)):
     title.append(titles_raw[i].text)
         
-'''for i in title:
-    if i[:10] == "Egyveleg –":
-        ch = "Egyveleg"
-        channel_1half = channel[:i]
-        channel_2half = channel[i:]
-        channel = channel_1half + ch + channel_2half'''
-
-
     
 views_raw = bs.findAll('span', {'class': 'style-scope ytd-video-meta-block'})
+#Handling some special cases with the views. The number of views and the time past since upload has the same tags and class 
 for i in range(len(views_raw)):
+    #if the video is a live stream, then the page doesn't show since when the video has been uploaded,
+    #to handle this I appended 'élő' to the views text
     if "aktív néző" in views_raw[i].text:
         views.append(views_raw[i].text)
         views.append('élő')
+    #if the video is a stream before premier without any waiting user, we have to write something to the number of views,
+    #to handle this I appended 'premier előtt'
     elif "Premier" in views_raw[i].text and "várakozik" not in views_raw[i-1].text:
         views.append('premier előtt')
         views.append(views_raw[i])
+    #and if there are none of the special cases, then just simply append the text to the result list
     else:
         views.append(views_raw[i].text)
     #print(views_raw[i])
 
 view_final = []
 
-
+#we have to divide the views list to 2 parts, starting from the first element, every second element is the number of views
 for i in range(0,len(views),2):
     view_final.append(views[i])
 
+#this loop gets the time past since upload
 for i in range(1,len(views),2):
     upload.append(views[i])
 
@@ -84,11 +83,9 @@ for i in range(1,len(views),2):
 final = pd.DataFrame()
 #final1 = pd.DataFrame()
 
-'''for i in views:
-    print(i)'''
-print(len(channel))
+'''print(len(channel))
 print(len(title))
-print(len(views))
+print(len(views))'''
 
 final['channel'] = channel
 final['title'] = title
